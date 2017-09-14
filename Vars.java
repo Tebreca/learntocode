ae625ef45289f46df02f982b7707ec669a4c6872
Tebreca <ewan.arends10@gmail.com>
2017-09-14 18:23:40 +0200

    Update Vars.java

--- Vars.java
+++ Vars.java
@@ -1,7 +1,7 @@
 Public static void main(String[] args){
-  Private Boolean Hoi = true;
-  Private Int Nummer = 12;
+  Private Boolean hello = true;
+  Private Int Number = 12;
   
-  system.out.printLn(Nummer)
-  system.out.printLn(Hoi)
+  system.out.printLn(Number)
+  system.out.printLn(hello)
 }
